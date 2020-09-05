9f6577cbbb62825ed291d7fb13b59e73d4118c6a
tmecco <takeyoshi0613@icloud.com>
2020-09-05 12:13:51 +0900

    ChatSpace自動更新機能の実装

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="MessageBox">
+        `<div class="MessageBox" data-message-id=${message.id}>
           <div class="MessageInfo">
             <div class="MessageInfo__userName">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="MessageBox">
+      `<div class="MessageBox" data-message-id=${message.id}>
         <div class="MessageInfo">
           <div class="MessageInfo__userName">
             ${message.user_name}
@@ -39,6 +39,7 @@ $(function(){
       return html;
     };
   }
+
 
   $(".Form").on('submit', function(e){
     e.preventDefault(); //デフォルトのイベントを止める
@@ -65,10 +66,36 @@ $(function(){
       $('.Form__submit').prop('disabled', false);
     });
   });
+
+  //   let reloadMessages = function() {
+  //     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  //     let last_message_id = $('.MessageBox:last').data("message-id") || 0;
+  //     $.ajax({
+  //       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+  //       url: "api/messages",
+  //       //ルーティングで設定した通りhttpメソッドをgetに指定
+  //       type: 'get',
+  //       dataType: 'json',
+  //       //dataオプションでリクエストに値を含める
+  //       data: {id: last_message_id}
+  //     })
+  //     .done(function(messages) {
+  //       // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+  //       if (messages.length !== 0) {
+  //         //追加するHTMLの入れ物を作る
+  //         let insertHTML = '';
+  //         //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+  //         $.each(messages, function(i, message) {
+  //           insertHTML += buildHTML(message)
+  //         });
+  //         //メッセージが入ったHTMLに、入れ物ごと追加
+  //         $('.MessageField').append(insertHTML);
+  //         $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
+  //       }
+  //     })
+  //     .fail(function() {
+  //       alert('error');
+  //     });
+  //   }; 
+  //   setInterval(reloadMessages, 7000);
 });
-
-
-// $('.MessageField').append(html);
-//       $('form')[0].reset();
-//       $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
-//       // $('.Form__submit').prop('disabled', false);
